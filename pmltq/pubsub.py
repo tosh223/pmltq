7c8b38c8f377af7375753315f5f0c190062449cf
tosh223 <rev.to12@gmail.com>
2021-02-27 12:20:28 +0900

    update pubsub

--- pmltq/pubsub.py
+++ pmltq/pubsub.py
@@ -1,12 +1,22 @@
+import os
+from datetime import datetime
+
 from google.cloud import monitoring_v3
 from google.cloud.monitoring_v3 import query
 
-project = "my-project"
+project = os.environ['GCP_PROJECT']
 client = monitoring_v3.MetricServiceClient()
-result = query.Query(
-         client,
-         project,
-         'pubsub.googleapis.com/subscription/num_undelivered_messages', 
-         minutes=60).as_dataframe()
+pubsub_query = query.Query(
+    client,
+    project,
+    'pubsub.googleapis.com/subscription/num_undelivered_messages',
+    end_time=datetime.now(),
+    minutes=2   # if set 1 minute, we get nothing while creating the latest metrics.
+)
+# .select_resources(subscription_id=sub_name)
 
-print(result['pubsub_subscription'][project]['subscription_name'][0])
+for content in pubsub_query:
+    print(content)
+    # subscription_id = content.resource.labels['subscription_id']
+    # count = content.points[0].value.int64_value
+    # print(f'{subscription_id}: {count}')
