8e8618f2d7cc4e7eff81f0f20991b1daa31712b8
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-03-17 22:44:49 +0900

    subscriber has priorities

--- queuing_hub/subscriber.py
+++ queuing_hub/subscriber.py
@@ -3,13 +3,19 @@ from queuing_hub.connector.aws import AwsSubscriber as Aws
 from queuing_hub.connector.gcp import GcpSubscriber as Gcp
 
 class Subscriber:
+
+    connectors: list(Base) = [Aws(), Gcp()]
+
     def __init__(self):
-        self.__connectors: list(Base) = [Aws(), Gcp()]
-        self.__subscriptions = {}
+        self.__sub_conf = {}
+        self.__sub_list = []
+        # type, path, priority
+
+        self.__get_priority_sub()
 
     def qsize(self):
         response = {}
-        for connector in self.__connectors:
+        for connector in self.connectors:
             response.update(connector.qsize())
         return response
 
@@ -17,21 +23,26 @@ class Subscriber:
         response = {}
         connector: Base
 
-        # primary subscriber
-        values = [x['path'] for x in self.__subscriptions if x['priority'] == 0]
-        primary = values[0] if values else None
-        connector = self.__get_connector(primary)
-        if connector is None:
-            return None
-        response = connector.get(primary, max_num)
-
-        # secondary subscriber
+        for sub in self.__sub_list:
+            connector = self.__get_connector(sub['type'])
+            if connector is None:
+                raise Exception()
+            response = connector.get(sub['path'], max_num)
+            if response != {}:
+                break
 
         return response
 
+    def __get_priority_sub(self):
+        for i, key in enumerate(self.__sub_conf):
+            subs = [x['path'] for x in self.__sub_conf if x['priority'] == i]
+            sub = subs[0] if subs else None
+            if sub:
+                self.__sub_list.append(self.__sub_conf[key])
+
     @staticmethod
-    def __get_connector(subscription):
-        conn_type = str(subscription['type']).upper()
+    def __get_connector(type_str: str):
+        conn_type = str(type_str).upper()
         if conn_type == 'AWS':
             connector = Aws()
         elif conn_type == 'GCP':
