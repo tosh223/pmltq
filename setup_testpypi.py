8f31d4976ad5b9e0b179ee0b4736db9d245597e5
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-04-01 22:17:57 +0900

    add setup_testpypi

---
+++ setup_testpypi.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='sfb',  # Required
+    version='0.0.0',  # Required
+    description='Multi-cloud Queuing Hub',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional
+    url='https://github.com/tosh223/py-queuing-hub',  # Optional
+    author='Toshifumi Tsutsumi',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='aws, gcp, queue, development',  # Optional
+    # package_dir={'': 'sfb'},  # Optional
+    packages=find_packages(),  # Required
+    python_requires='>=3.6, <3.10',
+    install_requires=['google-cloud-pubsub', 'google-cloud-monitoring', 'boto3'],  # Optional
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+    # package_data={  # Optional
+    #     'sample': ['package_data.dat'],
+    # },
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+    # project_urls={  # Optional
+    #     'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+    #     'Funding': 'https://donate.pypi.org',
+    #     'Say Thanks!': 'http://saythanks.io/to/example',
+    #     'Source': 'https://github.com/pypa/sampleproject/',
+    # },
+)
